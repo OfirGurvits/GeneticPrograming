c24595b5b5a88070b68f7fd325155d6ef74df008
OfirGurvits <ofirgurvits@gmail.com>
2024-03-06 19:48:08 +0200

    works?

--- evolutionary_programing/Generic_Evoluion.py
+++ evolutionary_programing/Generic_Evoluion.py
@@ -2,50 +2,62 @@
 # chromosomes
 import random
 
-
-class Generic_Evoluion:
+class GenericEvoluion:
     def __init__(self, problem, num_of_gens, elitism):
         self.problem = problem
         self.num_of_gens = num_of_gens
         self.elitism = elitism
-
+        self.l = 0
     def do_one_generation(self):
-        population = self.problem.population
+        # population = self.problem.population
         fitness = {}
         total = 0
+        max_fitness = 0
+        average_fitness = 0
+        best_solution = []
         for chromosome in self.problem.chromosomes:
-            cur_fitness = self.problem.fitness(population)
-            fitness[chromosome] = cur_fitness
+            cur_fitness = self.problem.fitness(chromosome)
+            fitness[tuple(chromosome)] = cur_fitness
             total += cur_fitness
+            if cur_fitness > max_fitness:
+                best_solution = chromosome
+                max_fitness = cur_fitness
+            average_fitness += cur_fitness
+        average_fitness /= len(self.problem.chromosomes)
+        print("average fitness is", average_fitness)
+        print("max fitness is", max_fitness)
+        print("best solution is", best_solution)
+        print("generation: ", self.l)
+        self.l += 1
         self.crossover(total, fitness)
 
     def get_parent(self, fitness, number, chromosomes):
         for chromosome in chromosomes:
-            if fitness[chromosome] > number:
+            if fitness[tuple(chromosome)] > number:
                 return chromosome
-            number -= fitness[chromosome]
+            number -= fitness[tuple(chromosome)]
+        return chromosomes[-1]
 
     def crossover(self, total, fitness):
         num_of_chromosomes = len(self.problem.chromosomes)
         fitness_range = total
-        self.problem.chromosomes.sort(key=lambda chromosome: fitness[chromosome])
-        self.problem.chromosomes.reverse()
+        self.problem.chromosomes.sort(key=lambda chromosome: fitness[tuple(chromosome)], reverse=True)
 
         for i in range(self.elitism):
-            fitness_range -= fitness[self.problem.chromosomes[-1]]
+            fitness_range -= fitness[tuple(self.problem.chromosomes[-1])]
             # execute losers
             self.problem.chromosomes.pop()
         temp = self.problem.chromosomes.copy()
-        self.problem.chromosomes.empty()
+        self.problem.chromosomes = []
         for i in range(self.elitism):
             self.problem.chromosomes.append(temp[i])
 
         # do roulette and generate children
-        for i in range((num_of_chromosomes - self.elitism) / 2):
+        for i in range((num_of_chromosomes - self.elitism) // 2):
             random_number = random.randint(0, total)
-            parent1 = self.problem.get_parent(fitness, random_number, temp)
+            parent1 = self.get_parent(fitness, random_number, temp)
             random_number = random.randint(0, total)
-            parent2 = self.problem.get_parent(fitness, random_number, temp)
+            parent2 = self.get_parent(fitness, random_number, temp)
             child1, child2 = self.problem.crossover(parent1, parent2)
             self.problem.chromosomes.append(child1)
             self.problem.chromosomes.append(child2)
--- evolutionary_programing/QueensProblem.py
+++ evolutionary_programing/QueensProblem.py
@@ -1,9 +1,54 @@
+import random
+
+from Generic_Evoluion import GenericEvoluion
+
+
 class Queens_Evoluion:
+    def generateSolution(self):
+        solution = [1, 2, 3, 4, 5, 6, 7, 8]
+        random.shuffle(solution)
+        return solution
+
     def create_first_gen(self):
         for i in range(self.num_of_chromosomes):
-            self.chromosomes.append()
+            self.chromosomes.append(self.generateSolution())
 
-    def __init__(self, num_of_chromosomes):
+    def __init__(self, num_of_chromosomes, mutation_rate):
         self.chromosomes = []
         self.num_of_chromosomes = num_of_chromosomes
         self.create_first_gen()
+        self.mutation_rate = 0.01  # mutation_rate
+
+    def fitness(self, chromosome):
+        fitness = 0
+        for i in range(8):
+            for j in range(i + 1, 8):
+                if chromosome[i] != chromosome[j] and abs(i - j) != abs(chromosome[i] - chromosome[j]):
+                    fitness += 1
+        return fitness
+
+    def mutate(self, chromosome):
+        pos1, pos2 = random.sample(range(8), 2)
+        temp = chromosome[pos1]
+        chromosome[pos1] = chromosome[pos2]
+        chromosome[pos2] = temp
+        # return chromosome
+
+    def crossover(self, parent1, parent2):
+        child1 = []
+        child2 = []
+        crossover_point = random.randint(0, 7)
+        for i in range(8):
+            if i > crossover_point:
+                child1.append(parent1[i])
+                child2.append(parent2[i])
+            else:
+                child2.append(parent1[i])
+                child1.append(parent2[i])
+        return child1, child2
+
+
+if __name__ == "__main__":
+    queens = Queens_Evoluion(200, 0.1)
+    solution = GenericEvoluion(queens, 500, 4)
+    solution.run()
